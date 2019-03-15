7da63ebe1028dbd705e3b0a7d0946be26ef4c40c
Marijus <m.jenulis@gmail.com>
2019-03-15 21:34:43 +0200

    Initial Setup and comments

---
+++ routes/api/customers.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router({mergeParams: true});
+
+// Kai URL bus /api/customers grazins tuos duomenis JSON formatu
+// req - /api/customer route requestas galima paimt data is formu pvz. req.params 
+// res - response ka grazina/daro kai gauna request i sita route. Siuo atveju grazina JSON objekta
+router.get('/api/customers', (req, res) => {
+    const customers = [
+      {id: 1, firstName: 'John', lastName: 'Doe'},
+      {id: 2, firstName: 'Brad', lastName: 'Traversy'},
+      {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+    ];
+  
+    res.json(customers);
+  });
+
+// exportina routes
+module.exports = router;

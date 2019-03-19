0f773c13103b97feb95395575f730c4c8cda064d
Marijus <m.jenulis@gmail.com>
2019-03-19 11:23:44 +0200

    a

--- client/src/App.js
+++ client/src/App.js
@@ -10,17 +10,14 @@ import Register from './components/Register/Register';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React Express Starter</h1>
-        </header>
-        <Customers />
+      <Router>
+        <div>
+          <Route exact path="/" component={home} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/register" component={RegisterPage} />
 
-        
-        <Register/>
-        <Login />
-      </div>
+        </div>
+      </Router>
     );
   }
 }

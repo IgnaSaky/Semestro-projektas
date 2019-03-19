cd7685f0372bc191ce6cfd91fb0d882507345a14
Marijus <m.jenulis@gmail.com>
2019-03-19 23:11:02 +0200

    Bandziau pazaist su Axios

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -7,11 +7,39 @@ import axios from 'axios';
 export default class Login extends Component {
   constructor(props) {
     super(props);
+
+    this.OnChangeEmail = this.onChangeEmail.bind(this);
+    this.OnChangePassword = this.onChangePassword.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
     this.state = {
       email: '',
       password: '',
       rememberMe: ''
     }
+  }
+  onChangeEmail(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+  onChangePassword(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    const data = {
+      email: this.state.email,
+      password: this.state.password
+    }
+    axios.post('localhost:5000/login', data).then(res => console.log(res.data));
+
+    this.setState({
+      email: '',
+      password: ''
+    });
   }
   render() {
     return (
@@ -22,14 +50,14 @@ export default class Login extends Component {
                     <div className="card card-body fix-logo-translate">
                         <img className="user-logo" src={avatar} alt='user icon'/>
                         <h1 className="text-center mb-3"><i className="fas fa-sign-in-alt"></i> Prisijunk!</h1>
-                        <form action="" method="POST">
+                        <form onSubmit={this.onSubmit} action="/login" method="POST">
                             <div className="form-group">
                                 <label htmlFor="email">El. paštas</label>
-                                <input type="email" name="email" className="form-control" placeholder="El. pašto adresas"/>
+                                <input onChange={this.onChangeEmail} type="email" name="email" className="form-control" placeholder="El. pašto adresas"/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="password">Slaptažodis</label>
-                                <input type="password" id="password" name="password" className="form-control" placeholder="Slaptažodis"/>
+                                <input onChange={this.onChangePassword} type="password" id="password" name="password" className="form-control" placeholder="Slaptažodis"/>
                             </div>
                             <div className="form-group form-check">
                               <input type="checkbox" className="form-check-input" id='rememberMe'/>

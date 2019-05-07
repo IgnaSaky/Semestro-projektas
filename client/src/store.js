7ed3ae27f206372554f03831ebb6ede9064cfb6f
Marijus <m.jenulis@gmail.com>
2019-05-07 11:14:26 +0300

    chrome redux dev tools config

--- client/src/store.js
+++ client/src/store.js
@@ -6,11 +6,13 @@ const initialState = {};
 
 const middleWare = [thunk];
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
   initialState,
-  composeEnhancers(applyMiddleware(...middleWare))
+  compose(
+    applyMiddleware(...middleWare),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 export default store;

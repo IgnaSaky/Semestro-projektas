8a1592480dd4cdce6ccce1abd26ac534f45fff43
Marijus <m.jenulis@gmail.com>
2019-05-07 12:26:40 +0300

    .

--- client/src/components/Profile/Jumbtron.jsx
+++ client/src/components/Profile/Jumbtron.jsx
@@ -3,12 +3,12 @@ import './css/Profile.css';
 
 
 function Jumbtron(props) {
-
+  console.log(props.user);
   return (
     <div>
       <div className="container-fluid">
         <div className="jumbotron blue">
-            <h1 className="display-4">Sveiki, { "\"username\""}</h1>
+            <h1 className="display-4">Sveiki, { }</h1>
             <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
             <hr className="my-4"></hr>
             <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>

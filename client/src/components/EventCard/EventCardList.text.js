d7e8133cc49c24893023e871adc196c7210bb1f5
DomantasMk <45693077+DomantasMk@users.noreply.github.com>
2019-05-21 10:46:55 +0300

    event card list test

---
+++ client/src/components/EventCard/EventCardList.text.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import EventCardList from './EventCardList.jsx';
+import { shallow } from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+configure({ adapter: new Adapter() });
+
+
+it('Event card list renders without crashing', () => {
+   const div = document.createElement('div');
+   ReactDOM.render((
+    <Router>
+        <div>
+         <EventCardList/>
+        </div>
+    </Router>
+
+   ), div);
+});
+
+it('Event card renders title and description areas', () => {
+    const editor =shallow(<EventCardList/>);
+    expect(editor.find('.container-fluid').length).toEqual(1);
+});
+
+// it('Event card renders the html output correctly', () => {
+//     const editor =shallow(<EventCard />);
+//     const realOutput = editor.find('.card').html();
+//     expect("").toEqual(realOutput);
+// })
